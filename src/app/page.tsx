f89020610c8fb818a3a2bf0e8e55d5c2ec38e0f7
Hao Duong <73717997+HaoDuong1141@users.noreply.github.com>
2023-10-18 16:17:11 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Slider from "@/components/Silder";
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+
+export default function Home() {
+    return (
+        <main>
+            <Slider />
+            <Featured />
+            <Offer />
+        </main>
+    );
+}
